caf5812007a521192c9cb2ff1b1079b05416781b
Alexander Pereyra <alexanderpereyra@Alexanders-MacBook-Pro.local>
2019-03-04 00:12:46 -0500

    clean up

--- get_workspace_list.py
+++ get_workspace_list.py
@@ -6,8 +6,3 @@ workspaceid = response['Workspaces']
 for id in workspaceid:
     workspaceid_list.append(workspaceid[0]['WorkspaceId'])
     print(workspaceid_list)
-
-counter = 0
-for c in client.describe_workspaces():
-    counter = counter + 1
-    print(counter)
